1cc1f14f221b508ff6b8a82a1418f9b8132d5cfa
mikegv <xmik3yvx@live.com>
2022-09-01 09:43:25 -0700

    added rules button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import './App.css';
-import Header from './components/Header';
-import GameArea from './components/GameArea';
-import { useState, useEffect } from 'react'
+import "./App.css";
+import Header from "./components/Header";
+import GameArea from "./components/GameArea";
+import { useState, useEffect } from "react";
+import RulesButton from "./components/RulesButton";
 
 function App() {
-  const [score, setScore] = useState(0)
+  const [score, setScore] = useState(0);
   const updateScore = (win) => {
-    if(win){
-      setScore(prev => prev + 1)
-    }else if(score > 0){
-      setScore(prev => prev - 1)
+    if (win) {
+      setScore((prev) => prev + 1);
+    } else if (score > 0) {
+      setScore((prev) => prev - 1);
     }
-  }
+  };
 
   return (
     <div className="App">
       <Header score={score} />
       <GameArea score={score} updateScore={updateScore} />
+      <div className="rules">
+        <RulesButton />
+      </div>
     </div>
   );
 }
---
+++ src/components/RulesButton.js
@@ -0,0 +1,9 @@
+import './rulesButton.css'
+
+const RulesButton = () => {
+    return (
+        <button className='rulesButton'>Rules</button>
+    );
+};
+
+export default RulesButton;
