7cf501856785450af76a9b88e07f8dd71ef4c146
mikegv <xmik3yvx@live.com>
2022-09-01 10:52:04 -0700

    change order of symbols on gameboard

--- src/components/GameArea.js
+++ src/components/GameArea.js
@@ -1,30 +1,51 @@
 import "./gameArea.css";
 import ImageIcon from "./ImageIcon";
-import { useState } from 'react'
+import { useState } from "react";
 import Result from "./Result";
 
-const GameArea = ({updateScore}) => {
-  const [playerHand, setPlayerHand] = useState(null)
+const GameArea = ({ updateScore }) => {
+  const [playerHand, setPlayerHand] = useState(null);
   const imageClickHandler = (hand, color) => {
-    setPlayerHand([hand, color])
-  }
+    setPlayerHand([hand, color]);
+  };
   const reset = () => {
-    setPlayerHand(null)
-  }
-  if(!playerHand){
-  return (
-    <div className="startScreen">
-      <ImageIcon color='red' symbol='rock' imageClickHandler={imageClickHandler} />
-      <ImageIcon color='purple' symbol='lizard' imageClickHandler={imageClickHandler} />
-      <ImageIcon color='lightblue' symbol='spock' imageClickHandler={imageClickHandler} />
-      <ImageIcon color='orange' symbol='scissors' imageClickHandler={imageClickHandler} />
-      <ImageIcon color='blue' symbol='paper' imageClickHandler={imageClickHandler} />
-    </div>
-  );
-  }else{
-    return(
+    setPlayerHand(null);
+  };
+  if (!playerHand) {
+    return (
+      <div className="startScreen">
+        
+        <ImageIcon
+          color="blue"
+          symbol="paper"
+          imageClickHandler={imageClickHandler}
+        />
+        <ImageIcon
+          color="red"
+          symbol="rock"
+          imageClickHandler={imageClickHandler}
+        />
+        <ImageIcon
+          color="purple"
+          symbol="lizard"
+          imageClickHandler={imageClickHandler}
+        />
+        <ImageIcon
+          color="lightblue"
+          symbol="spock"
+          imageClickHandler={imageClickHandler}
+        />
+        <ImageIcon
+          color="orange"
+          symbol="scissors"
+          imageClickHandler={imageClickHandler}
+        />
+      </div>
+    );
+  } else {
+    return (
       <Result playerHand={playerHand} updateScore={updateScore} reset={reset} />
-    )
+    );
   }
 };
 
